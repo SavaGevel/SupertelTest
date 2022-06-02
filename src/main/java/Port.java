f5434b3e925bfa60f0c763e3dbfd1cd7389d41a3
savelijgevel <gevel.saveliy@gmail.com>
2022-06-02 13:07:34 +0300

    constants were added

--- src/main/java/Port.java
+++ src/main/java/Port.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Port {
+    
+    private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
 
     private final List<String> indexes;
 
@@ -19,14 +22,14 @@ public class Port {
 
             List<Integer> numericalSequence = new ArrayList<>();
 
-            String[] numbers = index.split(",");
+            String[] numbers = index.split(COMMA);
 
             for (String number : numbers) {
                 number = number.trim();
                 if (number.length() == 1) {
                     numericalSequence.add(Integer.parseInt(number));
                 } else {
-                    String[] interval = number.split("[-–]");
+                    String[] interval = number.split(HYPHEN);
 
                     int leftBorder = Integer.parseInt(interval[0]);
                     int rightBorder = Integer.parseInt(interval[1]);
